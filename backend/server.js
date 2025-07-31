0f403a82dd34255f5ef6afa9ef2db35f4d398024
Avinash2002kanaujia <avinashkanaujia2219@gmail.com>
2025-07-31 19:16:25 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
+
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors({
+    origin: ['https://full-stack-forever-frontend.vercel.app', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
+
+// api endpoints
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
+app.use('/api/cart',cartRouter)
+app.use('/api/order',orderRouter)
+
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+// Export the app for serverless deployment
+export default app
